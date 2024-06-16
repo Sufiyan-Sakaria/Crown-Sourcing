215dba832eebb097c3541ce8b63e8028bcd99598
Sufiyan <sufiyansakaria2@gmail.com>
2024-06-16 13:16:42 +0500

    basic app

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+});
