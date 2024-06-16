f82c7f679c7872791c81eb21d242529c0248a058
Sufiyan <sufiyansakaria2@gmail.com>
2024-06-16 13:29:48 +0500

    v 0.1

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+require('dotenv').config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
