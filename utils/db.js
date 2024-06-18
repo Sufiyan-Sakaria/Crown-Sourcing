549478d97ece2b91f1eea12aa20276fd22151b5d
Sufiyan <sufiyansakaria2@gmail.com>
2024-06-18 18:26:21 +0500

    v 0.2.2

--- utils/db.js
+++ utils/db.js
@@ -1,18 +1,30 @@
+// import 
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
 
 // Function to connect to MongoDB
 const connectToDatabase = () => {
   const uri = process.env.MONGODB_URI; // Get MongoDB URI from environment variables
 
+  if (!uri) {
+    console.error("MONGODB_URI is not defined in the environment variables.");
+    return;
+  }
+
+  console.log("Connecting to MongoDB with URI:", uri); // Debugging statement
+
   mongoose
-    .connect(uri)
+    .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-      console.log("Database connected succesfully"); // Log success message
+      console.log("Database connected successfully"); // Log success message
     })
     .catch((err) => {
       console.error("Error connecting to Database:", err.message); // Log error message
     });
 };
 
-// export Mongoose connection
+// Export Mongoose connection function
 module.exports = connectToDatabase;
